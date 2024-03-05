d9a756ac3aa04c48dc1b3031ef6fe000ba5bd550
Juan <jotadebeese@gmail.com>
2024-03-05 19:11:56 +1100

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import NavMenu from '@/components/NavMenu'
 import '/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavMenu />
         {children}
+        <Analytics />
       </body>
     </html>
   )
