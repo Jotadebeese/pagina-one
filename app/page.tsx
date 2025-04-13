4f2769cf19579e0815ee4fc5622afd9c740d728e
Juan <jotadebeese@gmail.com>
2025-04-13 13:06:53 +0700

    Testing the branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import MainPageCard from '@/src/components/ui/MainPageCard';
-import styles from '@/src/styles/page.module.css';
+import MainPageCard from "@/src/components/ui/MainPageCard";
+import styles from "@/src/styles/page.module.css";
 
 export default async function Home() {
-
   return (
     <main className={styles.main}>
       <MainPageCard />
+      <h1>Hi</h1>
     </main>
   );
 }
